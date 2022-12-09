92ab3f33cdbf5de8f2fc3043de5a27f570aefb89
eemensch <iman.shames@anu.edu.au>
2022-12-09 15:13:20 +1100

    JT, theme

---
+++ content/seminars/JochenTrumpf.md
@@ -0,0 +1,15 @@
+---
+title: "Jochen Trumpf's talk on A Non-Linear Internal Model Principle for Observers"
+date: 2022-12-16T11:00:00+10:00
+draft: false
+---
+
+
+__Title:__  A Non-Linear Internal Model Principle for Observers
+
+
+__Time, Date, Location__: 11:00, Friday, 16 December, 2022, Brian Anderson Building Seminar Room
+
+__Abstract:__  We show that any asymptotic observer for a non-linear kinematic system on a differentiable manifold contains a full internal model of the plant. The result follows from an abstract, set-theoretic approximate internal model principle that generalises the known results for classical LTI systems, behavioural systems, and systems described by injective co-generator signal modules (Oberst’s algebraic analysis approach to system theory). At the core of the theory is the use of partially ordered set (poset) theory to generalise the classical LTI system notion of the anti-stabilizable part of a system. Most of the results in this talk were presented at the MTNS 2022 earlier this year.
+
+The work is done in collaboration with Johannes Nuessle
--- themes/blowfish/exampleSite/assets/js/home.js
+++ themes/blowfish/exampleSite/assets/js/home.js
@@ -2,8 +2,8 @@ var layouts = [
   "background",
   "hero",
   "profile",
-  "card",
-  "page"
+  "page",
+  "card"
 ]
 
 var currentLayout = 0
@@ -30,6 +30,47 @@ window.addEventListener("DOMContentLoaded", (event) => {
     button.addEventListener("click", function (e) {
       e.preventDefault();
       switchHomeLayout();
+    })
+  );
+});
+
+var list_config = [
+  "CardViewScreenWidth",
+  "CardViewProse",
+  "NormalView"
+]
+
+var titles = {
+  "CardViewProse" : "card view with constrained width",
+  "CardViewScreenWidth" : "card view with full width",
+  "NormalView" : "standard list view"
+}
+
+var currentConfig = 0
+
+function switchList() {
+
+  var old = currentConfig
+  currentConfig = currentConfig == list_config.length - 1 ? 0 : currentConfig + 1
+
+  var oldDiv = document.getElementById(list_config[old])
+  var currentDiv = document.getElementById(list_config[currentConfig])
+  const configCode = document.querySelectorAll("code[id=config]");
+
+  currentDiv.style.display = "block";
+  oldDiv.style.display = "none";
+
+  configCode.forEach(function (el) {
+    el.innerText = titles[list_config[currentConfig]];
+  });
+
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelectorAll("#switch-config-button").forEach((button) =>
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      switchList();
     })
   );
 });
